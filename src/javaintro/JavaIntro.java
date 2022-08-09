63c7fa746011dc4189eec3ce9705e7e8322c3345
Nuñez Francisco <francisconu1125@gmail.com>
2022-08-08 21:15:33 -0300

    Carga de la actividad Nro:1

--- src/javaintro/JavaIntro.java
+++ src/javaintro/JavaIntro.java
@@ -1,9 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Escribir un programa que pida dos números enteros por teclado y calcule la suma de los
+    dos. El programa deberá después mostrar el resultado de la suma 
  */
 package javaintro;
+
+import java.util.Scanner;
 
 /**
  *
@@ -15,6 +16,19 @@ public class JavaIntro {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        Scanner leer = new Scanner (System.in);
+        
+        int numeroA;
+        int numeroB;
+        
+        System.out.println("Ingrese el primero numero");
+        numeroA=leer.nextInt();
+        System.out.println("Ingrese el segundo numero");
+        numeroB=leer.nextInt();
+        
+        System.out.println("La suma de los dos numero es: "+(numeroA+numeroB));
+        
         // TODO code application logic here
     }
     
